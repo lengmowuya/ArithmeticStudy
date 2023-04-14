ab3144232209c018a04d1d49e61e76ddcf7d2fc6
lengmowuya <3537168410@qq.com>
2023-04-15 00:17:12 +0800

    弃用数组,大幅提升爬楼梯性能

--- JavaScript/15.爬楼梯.js
+++ JavaScript/15.爬楼梯.js
@@ -1,14 +1,13 @@
 var climbStairs = function(n) {
-    // let pre = [1,2];
-    // let count = 2;
-    // while(count < n){
-    //     pre.push(pre[0] + pre[1]);
-    //     pre.shift();
-    //     count++;
-    // }
-    const stair = (pre,now) => {
-        return n<3 ? [1,2][n-1] : stair();
+    let one = 1;
+    let two = 2;
+    let count = 2;
+    while(count < n){
+        let sum = one + two;
+        one = two;
+        two = sum;
+        count++;
     }
-    return n<3 ? [1,2][n-1] : stair();
+    return n<3 ? [1,2][n-1] : two;
 }
 console.log(climbStairs(5));
