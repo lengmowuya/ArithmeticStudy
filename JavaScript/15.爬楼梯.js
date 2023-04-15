7dcbdefa36dfe8dd42c6cad8d088019d3b19c79b
lengmowuya <3537168410@qq.com>
2023-04-15 11:46:13 +0800

    斐波那契数列的缓存递归思路get

--- JavaScript/15.爬楼梯.js
+++ JavaScript/15.爬楼梯.js
@@ -1,4 +1,6 @@
-var climbStairs = function(n) {
+// 70. 爬楼梯,给n级台阶,每次只能走1或2级,有多少种走法走完.
+// 1.使用斐波那契,快速计算
+/* var climbStairs = function(n) {
     let one = 1;
     let two = 2;
     let count = 2;
@@ -9,5 +11,18 @@ var climbStairs = function(n) {
         count++;
     }
     return n<3 ? [1,2][n-1] : two;
+} */
+
+
+// 缓存递归斐波那契数列
+// let cache = [];
+var climbStairs = (n)=>{
+    const fib = (n,cache)=>{
+        if(cache[n]) return cache[n];
+        const result = n<3 ? [1,2][n-1] : fib(n-1,cache) + fib(n-2,cache);
+        cache[n] = result;
+        return result;
+    }
+    return fib(n,[])
 }
-console.log(climbStairs(5));
+console.log(climbStairs(6,[]));
